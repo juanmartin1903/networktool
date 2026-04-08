9e75b6d3fd2618c9c7094d61641f2fb3a572ce0d
Juan Martin <jmart_159@hotmail.com>
2026-04-08 10:04:43 +0200

    feat: add menu c & python

---
+++ src/networktool.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main() {
+    printf("\n===NÄTVERKSVERKTYG===")
+    printf("1. Validera IP-adress")
+    printf("2. Validera port")
+    printf("3. Visa logg")
+    printf("4. Avsluta")
+}
+return 0;
---
+++ src/networktool.py
@@ -0,0 +1,9 @@
+
+def show_menu():
+    print("\n===NÄTVERKSVERKTYG===")
+    print("1. Validera IP-adress")
+    print("2. Validera port")
+    print("3. Visa logg")
+    print("4. Avsluta")
+
+show_menu()
